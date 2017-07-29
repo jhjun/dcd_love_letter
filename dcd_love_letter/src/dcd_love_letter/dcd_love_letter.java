5d8133d888ec242e8befa13a9bbbd9cf3050f056
yerung <yerung@yerung-PC>
2017-07-29 17:53:28 +0900

    basic flowchart

--- dcd_love_letter/src/dcd_love_letter/dcd_love_letter.java
+++ dcd_love_letter/src/dcd_love_letter/dcd_love_letter.java
@@ -2,18 +2,71 @@ package dcd_love_letter;
 
 public class dcd_love_letter {
 
-    public void init_game (int player_num) {
+	public int player_num;
+	public int turn;
+	
+    public void init_game () {
         
     }
 
-    public void init_round (int ){
-        
+    public void init_round (){
+        turn = 0;
     }
     
-    public int select_player
+    public int select_player () {
+    	int player;
+    	
+    	player = 0; // 0, 1, 2, 3
+    	
+        return player;
+    }
+    
+    public void player_action () {
+    	
+    }
+    
+    public void computer_action () {
+    	
+    }
+    
+    public boolean check_round_over () {
+    	return true;
+    }
+    
+    public boolean check_game_over () {
+    	return true;
+    }
     
     public static void main (String args[]) {
-        System.out.println("Hello World");
+    	
+    	dcd_love_letter game = new dcd_love_letter();
+
+    	game.player_num = 4; // need to be input
+
+    	game.init_game();
+
+        while (true)
+        {
+        	game.init_round();
+
+        	while (true)
+        	{
+        		int cur_player;
+
+        		cur_player = game.select_player();
+        		
+        		if (cur_player == 0)
+        			game.player_action();
+        		else
+        			game.computer_action();
+        		
+        		if (game.check_round_over())
+        			break;
+        	}
+        	
+        	if (game.check_game_over())
+        		break;
+        }
     }
 
 }
