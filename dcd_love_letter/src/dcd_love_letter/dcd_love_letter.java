831778f5afd213a1d241fd1a84eb9d9b31a6bec6
yerung <yerung@yerung-PC>
2017-07-29 17:24:01 +0900

    aaaaaa

--- dcd_love_letter/src/dcd_love_letter/dcd_love_letter.java
+++ dcd_love_letter/src/dcd_love_letter/dcd_love_letter.java
@@ -1,7 +1,19 @@
 package dcd_love_letter;
 
 public class dcd_love_letter {
-    public static void main(String args[]) {
+
+    public void init_game (int player_num) {
+        
+    }
+
+    public void init_round (int ){
+        
+    }
+    
+    public int select_player
+    
+    public static void main (String args[]) {
         System.out.println("Hello World");
     }
+
 }
